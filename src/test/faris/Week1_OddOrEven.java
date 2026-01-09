263caa2ed6a2f5b4ac1b85f1b4829b2a2cabab7b
rajae.ha <rajae.hamida@gmail.com>
2026-01-09 14:37:35 -0500

    CCreated the project

---
+++ src/test/faris/Week1_OddOrEven.java
@@ -0,0 +1,12 @@
+package test.faris;
+
+public class Week1_OddOrEven {
+    /**
+     * `Numbers -- odd & even
+     * Write a method which can identifies given number is even or odd
+     * EX:
+     * identify(5) -> "Odd"
+     * identify(6) -> "Even"
+     */
+
+}
